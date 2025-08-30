c02630769f0d71b26b8a007343e47c9a6da994f6
Blue <azuldebenedetti@gmail.com>
2025-08-30 19:07:00 +0100

    Update userRoutes.ts

--- mdx-student-union-backend/src/routes/userRoutes.ts
+++ mdx-student-union-backend/src/routes/userRoutes.ts
@@ -11,8 +11,8 @@ const router = express.Router();
 
 router.post("/", createUser);
 router.get("/:id", getUserById);
-router.put("/id", updateUser);
-router.get("/:id", getAllUsers);
+router.put("/:id", updateUser);
+router.get("/", getAllUsers);
 router.delete("/:id", deleteUser);
 
 export default router;
