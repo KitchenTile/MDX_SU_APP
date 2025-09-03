6bb9e88ac17c2ff45d7ad72eeb7a4c6479293f17
Blue <azuldebenedetti@gmail.com>
2025-09-03 23:50:47 +0100

    Update authMiddlewares.ts

--- mdx-student-union-backend/src/middleware/authMiddlewares.ts
+++ mdx-student-union-backend/src/middleware/authMiddlewares.ts
@@ -1,20 +1,29 @@
 import jwt from "jsonwebtoken";
 import { NextFunction, Request, Response } from "express";
 
+interface AuthRequest extends Request {
+  user?: string | jwt.JwtPayload | undefined;
+}
+
 export const validateToken = async (
-  req: Request,
+  req: AuthRequest,
   res: Response,
   next: NextFunction
 ) => {
   const auth = req.headers.authorization;
 
-  if (auth !== undefined) {
-    const token = auth.replace("Bearer ", "");
-
-    jwt.verify(token, process.env.JWT_SECRET!, (err, decoded) => {
-      if (err) {
-        return res.status(400).json({ message: "Invalid token" });
-      }
-    });
+  if (!auth) {
+    return res.status(401).json({ message: "No token provided" });
   }
+
+  const token = auth.replace("Bearer ", "");
+
+  jwt.verify(token, process.env.JWT_SECRET!, (err, decoded) => {
+    if (err) {
+      return res.status(401).json({ message: "Invalid token" });
+    } else {
+      req.user = decoded;
+      next();
+    }
+  });
 };
