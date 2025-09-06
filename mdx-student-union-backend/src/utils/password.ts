4f6af1c1a6ee00ae0bf03251bba7e209c979c57c
Blue <azuldebenedetti@gmail.com>
2025-09-06 22:25:17 +0100

    Update password.ts

--- mdx-student-union-backend/src/utils/password.ts
+++ mdx-student-union-backend/src/utils/password.ts
@@ -13,3 +13,16 @@ export const comparePassword = async (
 ) => {
   return bcrypt.compare(password, userPassword);
 };
+
+export function isPasswordValid(password: string): boolean {
+  /*
+   * At least one lowercase letter ((?=.*[a-z])).
+   * At least one uppercase letter ((?=.*[A-Z])).
+   * At least one digit ((?=.*\d)).
+   * Consists of only alphanumeric characters ([a-zA-Z\d]).
+   * Has a minimum length of 8 characters ({8,}).
+   */
+  const regex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/;
+
+  return regex.test(password);
+}
