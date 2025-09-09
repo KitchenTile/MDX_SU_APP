1319f7b910ded221be4b15bffd6469a734387dd4
Blue <azuldebenedetti@gmail.com>
2025-09-09 15:04:46 +0100

    Update db.ts

--- mdx-student-union-backend/src/config/db.ts
+++ mdx-student-union-backend/src/config/db.ts
@@ -2,11 +2,7 @@ import mongoose from "mongoose";
 
 export const connectDb = async () => {
   try {
-    // await mongoose.connect(process.env.MONGO_URI!);
-    await mongoose.connect(
-      "mongodb+srv://azuldebenedetti_db_user:wfrFRO9UEdCuXq4D@mdx-su-db.eyetz9f.mongodb.net/?retryWrites=true&w=majority&appName=MDX-SU-DB"
-    );
-
+    await mongoose.connect(process.env.MONGO_URI!);
     console.log("connected to db succesfully");
   } catch (error) {
     console.error("MongoDB connection error:", error);
