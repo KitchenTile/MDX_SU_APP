f4b10e8f2410e66d1258af38f94485c55e27a840
Blue <azuldebenedetti@gmail.com>
2025-09-14 14:17:53 +0100

    Create icons.tsx

---
+++ mdx-su-rn-app/constants/icons.tsx
@@ -0,0 +1,10 @@
+import { Feather, FontAwesome6, Ionicons } from "@expo/vector-icons";
+
+  export const icon = {
+    Events: (props: any) => <Ionicons name="calendar-outline" size={24} color="black" {...props}/>,
+    index: (props: any) =>  <Feather name='home' size={24} color={"#222"} {...props}/>,
+    explore: (props: any) => <FontAwesome6 name="newspaper" size={24} color="black" {...props}/>,
+
+    news: (props: any) => <FontAwesome6 name="newspaper" size={24} color="black" {...props}/>,
+    // profile: (props: any) =>  <Feather name='user' size={24} color={"#222"} {...props}/>,
+  }
