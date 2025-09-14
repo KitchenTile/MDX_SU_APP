65c172cd1f07e22bace1977d3ce6059bb259fd5b
KitchenTile <azuldebenedetti@gmail.com>
2025-09-14 20:40:19 +0100

    Create fetchAPI.ts

---
+++ mdx-su-rn-app/api/fetchAPI.ts
@@ -0,0 +1,41 @@
+import { baseURL } from "@/utils/api"
+
+export const postCommunication = async (body: Object) => {
+    try {
+        const res = await fetch(`${baseURL + "/posts/communication"}`, {
+            method: "POST",
+            headers: { "content-type": "application/json" },
+            body: JSON.stringify(body)
+        })
+         return res;
+    } catch(error) {
+        console.error(error)
+    }
+}
+export const postEvent = async (body: Object) => {
+    try {
+        const res = await fetch(`${baseURL + "/posts/event"}`, {
+            method: "POST",
+            headers: { "content-type": "application/json" },
+            body: JSON.stringify(body)
+        })
+         return res;
+    } catch(error) {
+        console.error(error)
+    }
+}
+
+export const getCommunications = async () => {
+    try {
+        const res = await fetch(`${baseURL + "/posts"}`)
+
+        if (!res.ok) {
+            console.log("Error fetching data")
+        }
+
+        const data = await res.json();
+        return data
+    } catch (error) {
+        console.error(error)
+    }
+}
