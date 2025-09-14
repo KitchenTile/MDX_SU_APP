cd2c0501e96fa7f419d3d58292ababb9d20fd74a
Blue <azuldebenedetti@gmail.com>
2025-09-14 14:18:15 +0100

    Create Events.tsx

---
+++ mdx-su-rn-app/app/(tabs)/Events.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const EventsPage = () => {
+  return (
+    <View>
+      <Text>Events</Text>
+    </View>
+  )
+}
+
+export default EventsPage
+
+const styles = StyleSheet.create({})
