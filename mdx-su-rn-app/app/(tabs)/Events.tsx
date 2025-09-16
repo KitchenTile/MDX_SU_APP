21fc381e682563c8b7dc213536e919960ccdaab8
KitchenTile <azuldebenedetti@gmail.com>
2025-09-16 22:16:26 +0100

    Update Events.tsx

--- mdx-su-rn-app/app/(tabs)/Events.tsx
+++ mdx-su-rn-app/app/(tabs)/Events.tsx
@@ -1,11 +1,11 @@
-import { StyleSheet, Text, View } from "react-native";
+import { ScrollView, StyleSheet, Text, View } from "react-native";
 import React from "react";
 
 const EventsPage = () => {
   return (
-    <View>
+    <ScrollView>
       <Text>Events</Text>
-    </View>
+    </ScrollView>
   );
 };
 
