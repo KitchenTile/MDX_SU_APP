f949c45f5de508ba6c8cd026c84a2e84889bd340
KitchenTile <azuldebenedetti@gmail.com>
2025-09-19 23:10:32 +0100

    Update NewsCard.tsx

--- mdx-su-rn-app/components/cards/NewsCard.tsx
+++ mdx-su-rn-app/components/cards/NewsCard.tsx
@@ -2,44 +2,74 @@ import { Image, StyleSheet, Text, TouchableOpacity, View } from "react-native";
 import React from "react";
 import { FontAwesome6 } from "@expo/vector-icons";
 import { formattedDate } from "@/utils/formattedDate";
+import {LinearGradient} from "expo-linear-gradient";
+
+
 
 const NewsCard = ({
   image,
   title,
   createdAt,
+  author
 }: {
   image: string;
   title: string;
   createdAt: string;
+  author: string
 }) => {
-  //   const formattedDate = createdAt.toLocaleDateString("en-GB", {
-  //     day: "numeric",
-  //     month: "short",
-  //     year: "numeric",
-  //   });
+
 
   const formattedCreatedAt = formattedDate(createdAt);
 
   return (
     <View style={styles.cardContainer}>
-      <Image
-        style={styles.cardImage}
-        source={
-          //   uri: image,
-          require("../../assets/images/Untitled_design-13.png")
-        }
-        resizeMode="cover"
-      />
-      <View style={styles.textContainer}>
-        <Text style={styles.title} numberOfLines={2} ellipsizeMode="tail">
-          {title}
-        </Text>
-        <Text style={{ fontSize: 13 }}>{formattedCreatedAt}</Text>
+      <View style={styles.imageTextContainer}>
+        <Image
+          style={styles.cardImage}
+          source={
+            //  { uri: image}
+            // require("../../assets/images/Untitled_design-13.png")
+            require("../../assets/images/Training-4.png")
+          }
+          resizeMode="cover"
+        />
+        <LinearGradient
+          colors={['transparent', "rgba(0,0,0,0.8)"]}
+          style={styles.background}
+        />   
+        <View style={styles.textContainer}>
+          <Text style={styles.title} numberOfLines={2} ellipsizeMode="tail">
+            {title}
+          </Text>
+        </View>
       </View>
+      <View styles={styles.footer}>
+                  <Text style={{ fontSize: 13, color: "white" }}>{formattedCreatedAt}</Text>
       <TouchableOpacity style={styles.arrowRight} onPress={() => {}}>
         <FontAwesome6 name="arrow-right" size={20} color="black" />
       </TouchableOpacity>
+      </View>
     </View>
+
+    //     <View style={styles.cardContainer}>
+    //   <Image
+    //     style={styles.cardImage}
+    //     source={
+    //       //  { uri: image}
+    //       require("../../assets/images/Untitled_design-13.png")
+    //     }
+    //     resizeMode="cover"
+    //   />
+    //   <View style={styles.textContainer}>
+    //     <Text style={styles.title} numberOfLines={2} ellipsizeMode="tail">
+    //       {title}
+    //     </Text>
+    //     <Text style={{ fontSize: 13 }}>{formattedCreatedAt}</Text>
+    //   </View>
+    //   <TouchableOpacity style={styles.arrowRight} onPress={() => {}}>
+    //     <FontAwesome6 name="arrow-right" size={20} color="black" />
+    //   </TouchableOpacity>
+    // </View>
   );
 };
 
@@ -54,24 +84,48 @@ const styles = StyleSheet.create({
     boxShadow: "rgba(0, 0, 0, 0.41) 0px 0px 10px 0px",
     marginBlock: 10,
     flexDirection: "row",
-    maxHeight: 200,
+    maxHeight: 300,
     overflow: "hidden",
+    flex: 1
+  },
+
+    background: {
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    top: 0,
+    height: "100%",
+  },
+
+  imageTextContainer: {
+    position: "relative",
+    backgroundColor: "black",
   },
   textContainer: {
     padding: 10,
     maxWidth: "65%",
+    position: "absolute",
+    bottom: 0,
+    left: 0,
   },
   cardImage: {
-    maxWidth: "35%",
-    height: 100,
-  },
+    maxWidth: "100%",
+    height: 150
+    },
 
   title: {
     fontSize: 16,
     fontWeight: "600",
     flexShrink: 1,
     marginBottom: 10,
+    color: "white",
   },
+
+  footer: {
+    height: "20%",
+    background: "#fff",
+  },
+
   arrowRight: {
     position: "absolute",
     right: 15,
